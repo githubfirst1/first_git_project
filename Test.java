383a8cad5c437dfc0c9a4958cefcd34b27db27ab
Bhupati Patel <bhupatipatel009@gmail.com>
2016-10-19 23:51:43 +0530

    Added fem methods

--- Test.java
+++ Test.java
@@ -3,4 +3,15 @@ public class HelloWorld {
       // Prints "Hello, World" in the terminal window.
       System.out.println("Hello, World");
    }
+   
+   private void display() {
+	  System.out.println("Hello, World Again");
+	  System.out.println("Hello, World Twice");
+   }
+   
+   private void display2() {
+	  System.out.println("Hello, World Again");
+	  System.out.println("Hello, World Thrice");
+   }
+   
 }
